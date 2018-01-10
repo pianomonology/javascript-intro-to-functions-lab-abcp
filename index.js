d1833c516eac2e7d7814cb8b91a41a2d33cedd41
Snel <snelson10@sfccmo.edu>
2018-01-10 04:45:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,3 +14,8 @@ function logWhisper(string){
   console.log(string.toLowerCase())
 }
 
+function sayHiToGrandma(string){
+  if (string === 'I love you, Grandma'){
+    return 'I love you, too.'
+  }
+}
